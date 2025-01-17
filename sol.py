311c10023129295d86532aeaa6152afa0329e9b8
aman-sapra_dtpl <aman.sapra@cred.club>
2025-01-18 04:24:58 +0530

    somedecketer

--- sol.py
+++ sol.py
@@ -2,5 +2,13 @@ import requests
 
 
 res = None
+secret = ""
+"""
+[default]
+aws_access_key_id = AKIAT4GVSAXXDHZEIEEU
+aws_secret_access_key = p455w0rd
+output = json
+region = us-east-2
+"""
 
 res = requests.get("http://xx22.com/a/x/e/l")
