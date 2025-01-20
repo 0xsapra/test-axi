114582302bb44689fa4e0f36806ef06554df90d7
aman-sapra_dtpl <aman.sapra@cred.club>
2025-01-21 00:37:18 +0530

    p4

--- sol.py
+++ sol.py
@@ -5,8 +5,8 @@ res = None
 secret = ""
 """
 [default]
-aws_access_key_id = AKIAT4GVSAXXDHZEIEEU
-aws_secret_access_key = Z86PT0M+6CWEX6VgxNiRTe2Wmnqk6+8O1$vvFxJG
+aws_access_key_id = AKIAT4GVSAXXKA4XDCSB
+aws_secret_access_key = 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
 output = json
 region = us-east-2
 """
