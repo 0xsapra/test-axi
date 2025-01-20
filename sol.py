e92b669133b859cafdf19f322be8244b0c9b1145
aman-sapra_dtpl <aman.sapra@cred.club>
2025-01-20 23:19:41 +0530

    pass3

--- sol.py
+++ sol.py
@@ -6,7 +6,11 @@ secret = ""
 """
 [default]
 aws_access_key_id = AKIAT4GVSAXXDHZEIEEU
+<<<<<<< HEAD
 aws_secret_access_key = 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
+=======
+aws_secret_access_key = Z86PT0M+6CWEX6VgxNiRTe2Wmnqk6+8O1avvExJE
+>>>>>>> 371b392 (pass)
 output = json
 region = us-east-2
 """
